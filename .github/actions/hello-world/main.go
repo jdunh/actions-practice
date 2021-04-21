a0012cb974c024d498d0e38128830ae3b9eb08ae
jdunham-infomagnus <56654406+jdunham-infomagnus@users.noreply.github.com>
2021-04-21 15:35:18 -0400

    Update main.go

--- .github/actions/hello-world/main.go
+++ .github/actions/hello-world/main.go
@@ -11,3 +11,14 @@ func main() {
 firstGreeting := os.Getenv("INPUT_FIRSTGREETING")
 secondGreeting := os.Getenv("INPUT_SECONDGREETING")
 thirdGreeting := os.Getenv("INPUT_THIRDGREETING")
+
+// Use those inputs in the actions logic
+fmt.Println("Hello " + firstGreeting)
+fmt.Println("Hello " + secondGreeting)
+
+// Someimes inputs are not "required" and we can build around that
+if thirdGreeting != "" {
+    fmt.Println("Hello " + thirdGreeting)
+    }
+
+}
